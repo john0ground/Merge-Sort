d6e5d1e4573e27450ffbff5fb8524371f9429423
John <mjohncorbet@gmail.com>
2023-08-30 22:57:03 +0800

    pseudocode for merge

---
+++ index.js
@@ -0,0 +1,16 @@
+//    MergePseudo
+
+//    setup empty array / sortedArray
+//    arguments = 1st Array / 2nd Array / 1st Array length / 2nd Array length
+//    set each initial index for 1st array, 2nd array, and sortedArray
+//    set loop condition = if the indexes of the 1st and 2nd array are less than their respective length
+
+//    then loop statement 
+//    if 1stArray[ index ] < 2ndArray[ index ], then push the former to sortedArray
+//    increment the index of 1st array and sortedArray
+
+//    else, push the latter to sortedArray
+//    increment index of 2nd array and sorted array
+
+//    close loop
+//    copy remaining elements with a for loop
