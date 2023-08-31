72a8f667332c3b32c3d1e40669080649911510b3
John <mjohncorbet@gmail.com>
2023-08-31 15:17:06 +0800

    refactor

--- index.js
+++ index.js
@@ -67,6 +67,50 @@ function mergeSort(array) {
     }
 }
 
-const array = [ 9, 3, 7, 5, 6, 4, 8, 2 ];
+// const array = [ 9, 3, 7, 5, 6, 4, 8, 2 ];
 // mergeSort(array);
-console.log(mergeSort(array));
+// console.log(mergeSort(array));
+
+
+const refactor = (() => {
+    const A = [ 2, 8, 15, 18, 19, 25 ];
+    const B = [ 5, 9, 12, 17 ];
+
+    function merge(A, B) {
+        const sortedArray = [];
+        let iA = 0, 
+            iB = 0
+    
+        while (iA < A.length && iB < B.length) {
+            A[iA] < B[iB] ?
+            sortedArray.push(A[iA++]):
+            sortedArray.push(B[iB++]);
+        }
+    
+        while (iA < A.length) {
+            sortedArray.push(A[iA++])
+        }
+    
+        while (iB < B.length) {
+            sortedArray.push(B[iB++])
+        }
+    
+        return sortedArray;
+    }
+
+    function mergeSort(array) {
+        if (array.length < 2) return array;
+            
+        const mid = Math.floor(array.length / 2);
+        const firstHalf = array.slice(0, mid);
+        const secondHalf = array.slice(mid);
+
+        return merge(
+            mergeSort(firstHalf),
+            mergeSort(secondHalf)
+        );
+    }
+
+    const array = [ 9, 3, 7, 5, 6, 4, 8, 2 ];
+    console.log(mergeSort(array));
+})();
